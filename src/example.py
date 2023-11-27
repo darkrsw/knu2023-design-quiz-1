f82ddaea3cecb72767a545f242c55d0f7b57fc70
Dongsun Kim <darkrsw@gmail.com>
2023-11-27 15:16:24 +0900

    Create example.py

---
+++ src/example.py
@@ -0,0 +1,32 @@
+class First():
+    def __init__(self):
+        print "first"
+
+class Second(First):
+    def __init__(self):
+        print "second"
+
+class Third(Second):
+    def __init__(self):
+        print "that's it"
+
+
+class A():
+    def __init__(self):
+        pass
+
+
+class B(A):
+    def __init__(self):
+        pass
+
+
+class C():
+    def __init__(self):
+        pass
+
+
+
+class D(A):
+    def __init__(self):
+        pass
