45cd0639a1717f5e9b9d4f7515a7e52e2c081577
Dongsun Kim <darkrsw@gmail.com>
2023-11-28 15:58:38 +0900

    Update example.py

--- src/example.py
+++ src/example.py
@@ -5,27 +5,22 @@ class First():
 class Second(First):
     def __init__(self):
         print "second"
-
+        
 class Third(Second):
     def __init__(self):
         print "that's it"
-
-
+        
 class A():
     def __init__(self):
         pass
-
-
+        
 class B(A):
     def __init__(self):
         pass
 
-
 class C():
     def __init__(self):
         pass
-
-
 
 class D(A):
     def __init__(self):
